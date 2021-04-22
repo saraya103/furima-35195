2e1bd62edaef19fdbcf399a349a80ea35d2501c2
saraya103 <saraya103@gmail.com>
2021-04-22 11:41:49 +0900

    edit function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -22,10 +22,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:item_name, :content, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :day_id, :price,
-                                 :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :content, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :day_id,
+                                 :price, :image).merge(user_id: current_user.id)
   end
 end
